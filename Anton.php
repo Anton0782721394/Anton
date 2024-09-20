03299bfcf34c5d296099b29710557ca601632095
Anton0782721394 <namaradicorine@gmail.com>
2024-09-20 14:16:27 +0300

    Anton.php

--- Anton.php
+++ Anton.php
@@ -1,3 +1,16 @@
-<?php
-echo"correct";
+<?php 
+$Name = "NAIJUKA ANTON";
+$DOB = "07/07/1999";
+$Address = "RWAMABONDO-NTUNGAMO";
+$current_year = 2024;
+$year_of_birth = 1999;
+$age = $current_year - $year_of_birth;
+$welcome = "Happy to see you back home";
+
+echo "Name: " . $Name . "\n";
+echo "Date of birth: " . $DOB . "\n";
+echo "Home Address: " . $Address . "\n";
+echo "Welcome! MR.ANTON, you are now " . $age . " years young.\n";
+echo "We are " . $welcome . "\n";
+
 ?>
